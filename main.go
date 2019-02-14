d6946cfb696e413fa5479c8604353f72d292f0a2
QiyanYu <qyu18@dons.usfca.edu>
2019-02-13 21:01:20 -0800

    fix bugs

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+
+	"./p1"
 )
 
 func main() {
-	/*mpt := new(p1.MerklePatriciaTrie)
+	mpt := new(p1.MerklePatriciaTrie)
 	mpt.Insert("a", "apple")
-	mpt.Insert("ab", "apple")*/
-	var hexArray []uint8
-	hexArray = []uint8{1, 2}
-	hexArray = hexArray[2:]
-	fmt.Println(hexArray)
+	mpt.Insert("b", "banana")
+	mpt.Insert("a", "new")
+	fmt.Println(mpt.Order_nodes())
+	// mpt.Delete("b")
+	// fmt.Println(mpt.Order_nodes())
 }
--- p1/MerklePatriciaTrie.go
+++ p1/MerklePatriciaTrie.go
@@ -1,5 +1,6 @@
-//package p1
-package main
+package p1
+
+//package main
 
 import (
 	"encoding/hex"
@@ -32,7 +33,7 @@ func (mpt *MerklePatriciaTrie) Get(key string) (string, error) {
 		return "", nil
 	}
 	var path = getHexArray(key)
-	var value = mpt.getHelper(mpt.db[mpt.root], path)
+	var value = mpt.getHelper(mpt.root, path)
 	if value == "" {
 		return "", errors.New("path_not_found")
 	} else {
@@ -40,7 +41,8 @@ func (mpt *MerklePatriciaTrie) Get(key string) (string, error) {
 	}
 }
 
-func (mpt *MerklePatriciaTrie) getHelper(node Node, path []uint8) string {
+func (mpt *MerklePatriciaTrie) getHelper(nodeHash string, path []uint8) string {
+	var node = mpt.db[nodeHash]
 	var nodeType = node.node_type
 	if nodeType == 0 {
 		return ""
@@ -49,7 +51,7 @@ func (mpt *MerklePatriciaTrie) getHelper(node Node, path []uint8) string {
 			if path[0] == uint8(16) {
 				return node.branch_value[16]
 			} else {
-				return mpt.getHelper(mpt.db[node.branch_value[path[0]]], path[1:])
+				return mpt.getHelper(node.branch_value[path[0]], path[1:])
 			}
 		} else {
 			return ""
@@ -71,15 +73,18 @@ func (mpt *MerklePatriciaTrie) getHelper(node Node, path []uint8) string {
 		var cpLen = len(commonPath)
 		var rpLen = len(restPath)
 		var rnLen = len(restNibble)
-		if cpLen != 0 && rpLen == 0 && rnLen == 0 {
-			if isLeaf {
+		if isLeaf {
+			if cpLen != 0 && rpLen == 0 && rnLen == 0 {
 				return nodeValue
 			} else {
-				var nextNode = mpt.db[nodeValue]
-				return mpt.getHelper(nextNode, restPath)
+				return ""
 			}
 		} else {
-			return ""
+			if cpLen != 0 && rnLen == 0 {
+				return mpt.getHelper(nodeValue, restPath)
+			} else {
+				return ""
+			}
 		}
 	}
 	return ""
@@ -94,35 +99,35 @@ func (mpt *MerklePatriciaTrie) Insert(key string, new_value string) {
 		mpt.root = rootNode.hash_node()
 	}
 	var path = getHexArray(key)
-	var rootNode = mpt.db[mpt.root]
-	mpt.root = mpt.insertHelper(rootNode, path, new_value)
+	mpt.root = mpt.insertHelper(mpt.root, path, new_value)
 }
-func (mpt *MerklePatriciaTrie) insertHelper(node Node, path []uint8, value string) string {
+func (mpt *MerklePatriciaTrie) insertHelper(nodeHash string, path []uint8, value string) string {
+	var node = mpt.db[nodeHash]
 	var nodeType = node.node_type
 	var nodeKey = node.hash_node()
 	if nodeType == 0 { //insert into Null
+		delete(mpt.db, nodeKey)
 		var rootNode Node
 		rootNode.node_type = 2
 		rootNode.flag_value.encoded_prefix = compact_encode(path)
 		rootNode.flag_value.value = value
 		var hashValue = rootNode.hash_node()
 		mpt.db[hashValue] = rootNode
-		delete(mpt.db, nodeKey)
 		return hashValue
 	} else if nodeType == 1 { //insert into Branch Node
 		if path[0] == uint8(16) { //if insert into branch node value, just update the value
+			delete(mpt.db, nodeKey)
 			node.branch_value[16] = value
 			mpt.db[node.hash_node()] = node
-			delete(mpt.db, nodeKey)
 			return node.hash_node()
 		}
 		if getBranchCommonPath(node.branch_value, path) { //exist common path
 			var commonPath = path[0]
-			var nextNode = mpt.db[node.branch_value[commonPath]]
-			node.branch_value[commonPath] = mpt.insertHelper(nextNode, path[1:], value)
+			var nextNodeHash = node.branch_value[commonPath]
+			node.branch_value[commonPath] = mpt.insertHelper(nextNodeHash, path[1:], value)
 			var nodeHashValue = node.hash_node()
-			mpt.db[nodeHashValue] = node
 			delete(mpt.db, nodeKey)
+			mpt.db[nodeHashValue] = node
 			return nodeHashValue
 		} else { // don't exist common path
 			var restPath = path[0]
@@ -130,17 +135,17 @@ func (mpt *MerklePatriciaTrie) insertHelper(node Node, path []uint8, value strin
 			newLeafNode.node_type = 2
 			newLeafNode.flag_value.value = value
 			newLeafNode.flag_value.encoded_prefix = compact_encode(path[1:])
+			delete(mpt.db, nodeKey)
 			mpt.db[newLeafNode.hash_node()] = newLeafNode
 			node.branch_value[restPath] = newLeafNode.hash_node()
 			var nodeHashValue = node.hash_node()
 			mpt.db[nodeHashValue] = node
-			delete(mpt.db, nodeKey)
 			return nodeHashValue
 		}
 	} else if nodeType == 2 { //insert into extension node or leaf node
 		var encodeValue = node.flag_value.encoded_prefix
 		var nodeValue = node.flag_value.value
-		var isLeaf = encodeValue[0] == uint8(2) || encodeValue[0] == uint8(3)
+		var isLeaf = isLeafNode(encodeValue)
 		if isLeaf { //insert into leaf node
 			var nodePath = append(compact_decode(encodeValue), uint8(16)) //since it is the leaf node, add 16 back
 			var commonPath = getExtLeafCommonPath(nodePath, path)
@@ -150,55 +155,85 @@ func (mpt *MerklePatriciaTrie) insertHelper(node Node, path []uint8, value strin
 			var rpLen = len(restPath)
 			var rnLen = len(restNibble)
 			if cpLen != 0 && rpLen == 0 && rnLen == 0 { //update the leaf node value
-				nodeValue = value
-				mpt.db[node.hash_node()] = node
+				node.flag_value.value = value
 				delete(mpt.db, nodeKey)
+				mpt.db[node.hash_node()] = node
 				return node.hash_node()
 			}
 			if cpLen != 0 && rpLen != 0 && rnLen != 0 { //has common path so 1)new extension node 2) new branch node 3)insert these two nodes
+				delete(mpt.db, nodeKey)
 				var newExtNode Node
 				newExtNode.node_type = 2
 				newExtNode.flag_value.encoded_prefix = compact_encode(commonPath)
 				var newBranchNode Node
-				mpt.insertHelper(newBranchNode, restPath, value)
-				newExtNode.flag_value.value = mpt.insertHelper(newBranchNode, restNibble, nodeValue)
+				newBranchNode.node_type = 1
+				mpt.db[newBranchNode.hash_node()] = newBranchNode
+				var newBranchNodeHash = newBranchNode.hash_node()
+				newBranchNodeHash = mpt.insertHelper(newBranchNodeHash, restPath, value)
+				newBranchNodeHash = mpt.insertHelper(newBranchNodeHash, restNibble, nodeValue)
+				newExtNode.flag_value.value = newBranchNodeHash
 				mpt.db[newExtNode.hash_node()] = newExtNode
-				delete(mpt.db, nodeKey)
 				return newExtNode.hash_node()
 			}
 			if cpLen == 0 && rpLen != 0 && rnLen != 0 { //doesn't have common path so 1)new branch node 2) insert two nodes into branch node
 				var newBranchNode Node
 				newBranchNode.node_type = 1
-				newBranchNode.branch_value[restPath[0]] = mpt.insertHelper(newBranchNode, restPath, value)
-				newBranchNode.branch_value[restNibble[0]] = mpt.insertHelper(newBranchNode, restNibble, nodeValue)
-				mpt.db[newBranchNode.hash_node()] = newBranchNode
-				delete(mpt.db, nodeKey)
-				return newBranchNode.hash_node()
+				var newBranchNodeHash = newBranchNode.hash_node()
+				mpt.db[newBranchNodeHash] = newBranchNode
+				newBranchNodeHash = mpt.insertHelper(newBranchNodeHash, restPath, value)
+				newBranchNodeHash = mpt.insertHelper(newBranchNodeHash, restNibble, nodeValue)
+				//mpt.db[newBranchNode.hash_node()] = newBranchNode
+				//delete(mpt.db, nodeKey)
+				return newBranchNodeHash
 			}
 		} else { //insert into extension node
 			var nodePath = compact_decode(encodeValue)
 			var commonPath = getExtLeafCommonPath(nodePath, path)
 			var restPath = getRestPath(path, commonPath)
-			var restNibble = getRestNibble(path, commonPath)
+			var restNibble = getRestNibble(nodePath, commonPath)
 			var cpLen = len(commonPath)
 			var rpLen = len(restPath)
 			var rnLen = len(restNibble)
 			if cpLen != 0 && rpLen != 0 && rnLen != 0 { // 1）new extension node 2)new branch node 3)insert two paths into branch node
+				delete(mpt.db, nodeKey)
 				var newExtNode Node
 				newExtNode.node_type = 2
 				newExtNode.flag_value.encoded_prefix = compact_encode(commonPath)
 				var newBranchNode Node
 				newBranchNode.node_type = 1
-				mpt.insertHelper(newBranchNode, restPath, value)
-				newExtNode.flag_value.value = mpt.insertHelper(newBranchNode, restNibble, nodeValue)
+				if rpLen > 1 {
+					var newLeafNode Node
+					newLeafNode.node_type = 2
+					newLeafNode.flag_value.value = value
+					newLeafNode.flag_value.encoded_prefix = compact_encode(restPath[1:])
+					mpt.db[newLeafNode.hash_node()] = newLeafNode
+					newBranchNode.branch_value[restPath[0]] = newLeafNode.hash_node()
+				} else {
+					newBranchNode.branch_value[16] = value
+				}
+				if rnLen > 1 {
+					var newNextExtNode Node
+					newNextExtNode.node_type = 2
+					newNextExtNode.flag_value.value = nodeValue
+					newNextExtNode.flag_value.encoded_prefix = compact_encode(restNibble[1:])
+					mpt.db[newNextExtNode.hash_node()] = newNextExtNode
+					newBranchNode.branch_value[restNibble[0]] = newNextExtNode.hash_node()
+				} else {
+					newBranchNode.branch_value[restNibble[0]] = nodeValue
+				}
+				mpt.db[newBranchNode.hash_node()] = newBranchNode
+				// mpt.db[newBranchNodeHash] = newBranchNode
+				// newBranchNodeHash = mpt.insertHelper(newBranchNodeHash, restPath, value)
+				// newBranchNodeHash = mpt.insertHelper(newBranchNodeHash, restNibble, nodeValue)
+				newExtNode.flag_value.value = newBranchNode.hash_node()
 				mpt.db[newExtNode.hash_node()] = newExtNode
-				delete(mpt.db, nodeKey)
 				return newExtNode.hash_node()
-			} else if cpLen != 0 && rpLen != 0 && rnLen != 0 { //directly insert rest path into next node
-				var nextNode = mpt.db[nodeValue]
-				nodeValue = mpt.insertHelper(nextNode, restPath, value)
-				mpt.db[node.hash_node()] = node
+			} else if cpLen != 0 && rpLen != 0 && rnLen == 0 { //directly insert rest path into next node
+				//var nextNode = mpt.db[nodeValue]
+				node.flag_value.value = mpt.insertHelper(nodeValue, restPath, value)
 				delete(mpt.db, nodeValue)
+				mpt.db[node.hash_node()] = node
+
 				return node.hash_node()
 			} else if cpLen == 0 && rpLen != 0 && rnLen == 1 { //1)new branch 2)insert branch
 				var newBranchNode Node
@@ -214,8 +249,9 @@ func (mpt *MerklePatriciaTrie) insertHelper(node Node, path []uint8, value strin
 					mpt.db[newLeafNode.hash_node()] = newLeafNode
 				}
 				newBranchNode.branch_value[restNibble[0]] = nodeValue
-				mpt.db[newBranchNode.hash_node()] = newBranchNode
 				delete(mpt.db, nodeKey)
+				mpt.db[newBranchNode.hash_node()] = newBranchNode
+
 				return newBranchNode.hash_node()
 			} else if cpLen == 0 && rpLen != 0 && rnLen > 1 { //1)new branch node 2)new extension node
 				var newBranchNode Node
@@ -231,13 +267,14 @@ func (mpt *MerklePatriciaTrie) insertHelper(node Node, path []uint8, value strin
 					mpt.db[newLeafNode.hash_node()] = newLeafNode
 				}
 				var newExtNode Node
-				newExtNode.node_type = 1
+				newExtNode.node_type = 2
 				newExtNode.flag_value.encoded_prefix = compact_encode(restNibble[1:])
 				newExtNode.flag_value.value = nodeValue
+				delete(mpt.db, nodeKey)
 				mpt.db[newExtNode.hash_node()] = newExtNode
 				newBranchNode.branch_value[restNibble[0]] = newExtNode.hash_node()
 				mpt.db[newBranchNode.hash_node()] = newBranchNode
-				delete(mpt.db, nodeKey)
+
 				return newBranchNode.hash_node()
 			}
 		}
@@ -246,11 +283,12 @@ func (mpt *MerklePatriciaTrie) insertHelper(node Node, path []uint8, value strin
 }
 
 func (mpt *MerklePatriciaTrie) Delete(key string) (string, error) {
-	// TODO
 	var path = getHexArray(key)
-	var isSuc bool
-	isSuc, _ = mpt.deleteHelper(mpt.db[mpt.root], path)
+
+	isSuc, hashValue := mpt.deleteHelper(mpt.root, path)
+
 	if isSuc {
+		mpt.root = hashValue
 		return "", nil
 	} else {
 		return "", errors.New("path_not_found")
@@ -267,7 +305,8 @@ func getArrayInBranchValue(branchValue [17]string) []int {
 	return result
 }
 
-func (mpt *MerklePatriciaTrie) deleteHelper(node Node, path []uint8) (bool, string) {
+func (mpt *MerklePatriciaTrie) deleteHelper(nodeHash string, path []uint8) (bool, string) {
+	var node = mpt.db[nodeHash]
 	var nodeType = node.node_type
 	var nodeKey = node.hash_node()
 	if nodeType == 0 { // delete at Null node
@@ -279,7 +318,7 @@ func (mpt *MerklePatriciaTrie) deleteHelper(node Node, path []uint8) (bool, stri
 			} else { // has common path but not delete the branch value, into recursion
 				var isSuc bool
 				var nextKey string
-				isSuc, nextKey = mpt.deleteHelper(mpt.db[node.branch_value[path[0]]], path[1:])
+				isSuc, nextKey = mpt.deleteHelper(node.branch_value[path[0]], path[1:])
 				if isSuc { //delete successfully
 					node.branch_value[path[0]] = nextKey
 				} else { //not found
@@ -288,16 +327,17 @@ func (mpt *MerklePatriciaTrie) deleteHelper(node Node, path []uint8) (bool, stri
 			}
 			//check if there is only one value in branch node remaining
 			if len(getArrayInBranchValue(node.branch_value)) > 1 { //do not need to balance
-				mpt.db[node.hash_node()] = node
 				delete(mpt.db, nodeKey)
+				mpt.db[node.hash_node()] = node
 				return true, node.hash_node()
 			} else if getArrayInBranchValue(node.branch_value)[0] == 16 { // it is a leaf node
 				var returnNode Node
 				returnNode.node_type = 2
 				returnNode.flag_value.value = node.branch_value[16]
 				returnNode.flag_value.encoded_prefix = compact_encode([]uint8{uint8(16)})
-				mpt.db[returnNode.hash_node()] = returnNode
 				delete(mpt.db, nodeKey)
+				mpt.db[returnNode.hash_node()] = returnNode
+
 				return true, returnNode.hash_node()
 			} else {
 				var returnNode Node
@@ -309,14 +349,16 @@ func (mpt *MerklePatriciaTrie) deleteHelper(node Node, path []uint8) (bool, stri
 					returnNode.node_type = 2
 					returnNode.flag_value.value = nextNode.flag_value.value
 					var nextNodeEncodeValue = nextNode.flag_value.encoded_prefix
-					var newValue = append([]uint8{uint8(branchIndex)}, compact_decode(nextNode.flag_value.encoded_prefix)...)
-					if nextNodeEncodeValue[0] == uint8(2) || nextNodeEncodeValue[0] == uint8(3) {
-						newValue = append(newValue, uint8(16))
+					var nextNodeDecodeValue = compact_decode(nextNodeEncodeValue)
+					var newPathValue = append([]uint8{uint8(branchIndex)}, nextNodeDecodeValue...)
+					if isLeafNode(nextNodeEncodeValue) {
+						newPathValue = append(newPathValue, uint8(16))
 					}
-					returnNode.flag_value.encoded_prefix = compact_encode(newValue)
-					mpt.db[returnNode.hash_node()] = returnNode
+					returnNode.flag_value.encoded_prefix = compact_encode(newPathValue)
 					delete(mpt.db, nodeKey)
 					delete(mpt.db, nextNodeKey)
+					mpt.db[returnNode.hash_node()] = returnNode
+
 					return true, returnNode.hash_node()
 				} else { // if next node is branch node, return it as extension node
 					returnNode.node_type = 2
@@ -333,7 +375,7 @@ func (mpt *MerklePatriciaTrie) deleteHelper(node Node, path []uint8) (bool, stri
 	} else if nodeType == 2 { //delete at leaf or extension node
 		var encodeValue = node.flag_value.encoded_prefix
 		var nodeValue = node.flag_value.value
-		var isLeaf = encodeValue[0] == uint8(2) || encodeValue[0] == uint8(3)
+		var isLeaf = isLeafNode(encodeValue)
 		if isLeaf {
 			var nodePath = append(compact_decode(encodeValue), uint8(16)) //since it is the leaf node, add 16 back
 			var commonPath = getExtLeafCommonPath(nodePath, path)
@@ -352,14 +394,14 @@ func (mpt *MerklePatriciaTrie) deleteHelper(node Node, path []uint8) (bool, stri
 			var nodePath = compact_decode(encodeValue)
 			var commonPath = getExtLeafCommonPath(nodePath, path)
 			var restPath = getRestPath(path, commonPath)
-			var restNibble = getRestNibble(path, commonPath)
+			var restNibble = getRestNibble(nodePath, commonPath)
 			var cpLen = len(commonPath)
 			var rpLen = len(restPath)
 			var rnLen = len(restNibble)
-			if cpLen != 0 && rpLen == 0 && rnLen == 0 {
+			if cpLen != 0 && rpLen != 0 && rnLen == 0 {
 				var isSuc bool
 				var nextKey string
-				isSuc, nextKey = mpt.deleteHelper(mpt.db[nodeValue], restPath)
+				isSuc, nextKey = mpt.deleteHelper(nodeValue, restPath)
 				if isSuc {
 					var nextReturnNode = mpt.db[nextKey]
 					var nextReturnNodeType = nextReturnNode.node_type
@@ -368,7 +410,7 @@ func (mpt *MerklePatriciaTrie) deleteHelper(node Node, path []uint8) (bool, stri
 						newNode.node_type = 2
 						var nextReturnNodeEncodeValue = nextReturnNode.flag_value.encoded_prefix
 						var newValue = append(compact_decode(encodeValue), compact_decode(nextReturnNodeEncodeValue)...)
-						if nextReturnNodeEncodeValue[0] == uint8(2) || nextReturnNodeEncodeValue[0] == uint8(3) {
+						if isLeafNode(nextReturnNodeEncodeValue) {
 							newValue = append(newValue, uint8(16))
 						}
 						var newEncodeValue = compact_encode(newValue)
@@ -464,8 +506,7 @@ func compact_decode(encoded_arr []uint8) []uint8 {
 	}
 	if hexArray[0] == 1 || hexArray[0] == 3 {
 		hexArray = hexArray[1:]
-	}
-	if hexArray[0] == 0 || hexArray[0] == 2 {
+	} else {
 		hexArray = hexArray[2:]
 	}
 	return hexArray
@@ -478,9 +519,9 @@ func isLeafNode(encodedArray []uint8) bool {
 		hexArray = append(hexArray, uint8(n/16))
 		hexArray = append(hexArray, uint8(n%16))
 	}
-	if hexArray[0] == 0 || hexArray[0] == 1 {
-		return false
-	}
+	//	if hexArray[0] == 0 || hexArray[0] == 1 {
+	//		return false
+	//	}
 	if hexArray[0] == 2 || hexArray[0] == 3 {
 		return true
 	}
@@ -515,7 +556,6 @@ func (node *Node) hash_node() string {
 func getHexArray(key string) []uint8 {
 	rawArray := []uint8(key)
 	n := len(rawArray)
-	//fmt.Println(rawArray)
 	var hexArray []uint8
 	for i := 0; i < n; i++ {
 		num := rawArray[i]
@@ -524,23 +564,6 @@ func getHexArray(key string) []uint8 {
 		hexArray = append(hexArray, n1, n2)
 	}
 	return append(hexArray, 16)
-}
-
-func main() {
-	pa := []uint8{2, 7, 9}
-	pp := []uint8{2}
-	//fmt.Println(compact_encode(pa)[0] == 7)
-
-	//var s [17]string
-	//s[2] = "adf"
-	//var ss uint8
-	//ss = 3
-	fmt.Println(getRestPath(pa, pp))
-	//fmt.Println(compact_decode(compact_encode(pa))[0] == 7)
-	//fmt.Println(compact_decode(pa))
-	//fmt.Println(getHexArray("do"))
-	//fmt.Println(compact_encode(getHexArray("do")))
-
 }
 
 func (node *Node) String() string {
